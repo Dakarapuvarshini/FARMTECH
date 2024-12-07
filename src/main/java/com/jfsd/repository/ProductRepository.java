ba4534fc019af3cc3ac525f89d75d9cb0816bc1a
varshini dakarapu <Dakarapuvarshini>
2024-12-08 01:58:33 +0530

    first commit

---
+++ src/main/java/com/jfsd/repository/ProductRepository.java
@@ -0,0 +1,16 @@
+package com.jfsd.repository;
+
+import java.util.List;
+import java.util.Optional;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.stereotype.Repository;
+
+import com.jfsd.model.Product;
+
+@Repository
+public interface ProductRepository extends JpaRepository<Product, Long> {
+
+	
+}
